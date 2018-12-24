c7b5549467138d1ed6068f8e192c2f1d6f420105
tom_glb <goodMorning_glb@atguigu.com>
2018-12-24 22:46:53 +0800

    修改主页html

--- db/migrate/20181220081933_add_degree_to_grades.rb
+++ db/migrate/20181220081933_add_degree_to_grades.rb
@@ -1,4 +1,4 @@
-class AddDegreeToGrades < ActiveRecord::Migration
+class AddDegreeToGrades < ActiveRecord::Migration[5.0]
   def change
     add_column :grades, :degree, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,73 +9,70 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
- 
-# ActiveRecord::Schema.define(version: 20181222061401) do  wsy for merge conflict
-ActiveRecord::Schema.define(version: 20181222055210) do 
+
+ActiveRecord::Schema.define(version: 2018_12_22_055210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "courses", force: :cascade do |t|
-    t.string   "name"
-    t.string   "course_code"
-    t.string   "course_type"
-    t.string   "teaching_type"
-    t.string   "exam_type"
-    t.string   "credit"
-    t.integer  "limit_num"
-    t.integer  "student_num",   default: 0
-    t.string   "class_room"
-    t.string   "course_time"
-    t.string   "course_week"
-    t.integer  "teacher_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.boolean  "open",          default: false
-  end
-
-  create_table "grades", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.integer  "grade"
+  create_table "courses", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "course_code"
+    t.string "course_type"
+    t.string "teaching_type"
+    t.string "exam_type"
+    t.string "credit"
+    t.integer "limit_num"
+    t.integer "student_num", default: 0
+    t.string "class_room"
+    t.string "course_time"
+    t.string "course_week"
+    t.integer "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "degree"
+    t.boolean "open", default: false
+    t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
-  add_index "grades", ["course_id"], name: "index_grades_on_course_id", using: :btree
-  add_index "grades", ["user_id"], name: "index_grades_on_user_id", using: :btree
+  create_table "grades", id: :serial, force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "user_id"
+    t.integer "grade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "degree"
+    t.index ["course_id"], name: "index_grades_on_course_id"
+    t.index ["user_id"], name: "index_grades_on_user_id"
+  end
 
-  create_table "notices", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "content"
+  create_table "notices", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notices_on_user_id"
+  end
+
+  create_table "opensystems", id: :serial, force: :cascade do |t|
+    t.boolean "isopen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "notices", ["user_id"], name: "index_notices_on_user_id", using: :btree
-
-  create_table "opensystems", force: :cascade do |t|
-    t.boolean  "isopen"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "num"
+    t.string "major"
+    t.string "department"
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.boolean "teacher", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "num"
-    t.string   "major"
-    t.string   "department"
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.boolean  "teacher",         default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
